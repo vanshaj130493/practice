8c1aba418b05f47a6c5604c41c4fdc35923364c3
Vanshaj <vanshaj13@gmail.com>
2018-01-20 21:00:01 +0530

    add code

--- database/dbconnect.js
+++ database/dbconnect.js
@@ -9,7 +9,7 @@ client.connect();
 
 module.exports.executequery= function(req,response){
 
-client.query("SELECT * from test where id='abc';", (err, res) => {
+client.query("SELECT * from test;", (err, res) => {
   if (err) throw err;
   for (let row of res.rows) {
     res.end(JSON.stringify(row));
