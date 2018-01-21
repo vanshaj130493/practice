310ca38e8b6f50ab21e45bf915d0c1bc5e2124b8
Vanshaj <vanshaj13@gmail.com>
2018-01-22 04:04:46 +0530

    final commit

--- resources/test.js
+++ resources/test.js
@@ -6,7 +6,7 @@ module.exports.execute= function(req,res){
 	console.log("Request Received");
 	//db.executequery("create table kitchen (id serial, name varchar(200), quantity int, created_till_now int, predicted int);",function(output){
 		//console.log(output);
-		db.executequery("select * from kitchen;",function(output){
+		db.executequery("delete from kitchen;",function(output){
 		console.log(output);
 	});
 	//});
