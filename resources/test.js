0043dc7ab1d75075a396ac5421c9866954fd1b0e
Vanshaj <vanshaj13@gmail.com>
2018-01-21 21:55:18 +0530

    add code

--- resources/test.js
+++ resources/test.js
@@ -4,11 +4,11 @@ var db = require(path.join(__dirname, "..", "database", "dbconnect.js"));
 
 module.exports.execute= function(req,res){
 	console.log("Request Received");
-	db.executequery("create table kitchen (id serial, name varchar(200), quantity int, created_till_now int, predicted int);",function(output){
-		console.log(output);
+	//db.executequery("create table kitchen (id serial, name varchar(200), quantity int, created_till_now int, predicted int);",function(output){
+		//console.log(output);
 		db.executequery("select * from kitchen;",function(output){
 		console.log(output);
 	});
-	});
+	//});
 	res.end("Success !!")
 }
