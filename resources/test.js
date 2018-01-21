51a31650d373726b6d86cd4e061aafa53079e062
Vanshaj <vanshaj13@gmail.com>
2018-01-21 20:59:08 +0530

    add code

--- resources/test.js
+++ resources/test.js
@@ -4,7 +4,7 @@ var db = require(path.join(__dirname, "..", "database", "dbconnect.js"));
 
 module.exports.execute= function(req,res){
 	console.log("Request Received");
-	db.executequery("select * from test",function(output{
+	db.executequery("select * from test",function(output){
 		res.end(output);
 	});
 }
